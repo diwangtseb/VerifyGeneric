33fa44b0b3c381e1f14cebbefb33b8056d1b876b
wangdi <wangdi@lidarongyi.cn>
2020-03-03 19:30:37 +0800

    Nlog使用

---
+++ Nlog使用/Nlog使用/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace Nlog使用
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var loggerHelper=new LoggerHelper();
+            loggerHelper.Info("这他妈是啥啊");
+        }
+    }
+}
